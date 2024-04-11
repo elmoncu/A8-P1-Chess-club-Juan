cf46b2e46da329d2216cb5b23be547644d39353c
elmoncu <164093323+elmoncu@users.noreply.github.com>
2024-04-11 04:41:47 +0000

    function clicksubmitted()  js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function myFunction() {
-var x = document.getElementById("myInput");
-if (x.type === "password") {
-  x.type = "text";
-} else {
-  x.type = "password";
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
+
+function clicksubmitted() {
+  prompt("Your data have been submitted succesfully");
 }
