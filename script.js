0a9bd3aecff3f17e9beab7d9298aa81d00745df7
elmoncu <164093323+elmoncu@users.noreply.github.com>
2024-04-19 01:50:25 +0000

    change function upperletters

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-function myFunction() {
-  var x = document.getElementById("password");
-  if (x.type === "password") {
-    x.type = "text";
-  } else {
-    x.type = "password";
-  }
+var txt = document.getElementById("state");
+var x = document.getElementById("password").value;
+var checkBox = document.getElementById("myCheck");
+
+
+function upperletters() { 
+  x = txt.upppercase(); 
+  print(x)
 }
 
-function clicksubmitted() {
-  prompt("Your data have been submitted succesfully");
-}
+  // If the checkbox is checked, display the output password
+  function myFunction() {
+    if (checkBox.checked == true) {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+  function clicksubmitted() {
+    prompt("Your data have been submitted succesfully");
+  }
