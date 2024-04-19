046bf59f9a9d8c54f6cc9f031233169ab75ee01f
elmoncu <164093323+elmoncu@users.noreply.github.com>
2024-04-19 04:44:55 +0000

    input[type=reset] and input[type=submit] CSS

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-var txt = document.getElementById("state");
-var x = document.getElementById("password").value;
+var stateUp = document.getElementById("state").value;
+var x = document.getElementById("myInput");
 var checkBox = document.getElementById("myCheck");
 
-
-function upperletters() { 
-  x = txt.upppercase(); 
-  print(x)
+function upperCase() {
+  stateUp.value = state.value.toUpperCase();
+}
+// If the checkbox is checked, display the output password
+function myFunction() {
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
 }
 
-  // If the checkbox is checked, display the output password
-  function myFunction() {
-    if (checkBox.checked == true) {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
-  }
-
-  function clicksubmitted() {
-    prompt("Your data have been submitted succesfully");
-  }
+function clicksubmitted() {
+  prompt("Your data have been submitted succesfully");
+}
