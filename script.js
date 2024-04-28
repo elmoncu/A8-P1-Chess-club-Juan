2184e50c5444b270652bc3d9c437d3b58d581ea3
elmoncu <164093323+elmoncu@users.noreply.github.com>
2024-04-28 16:46:16 +0000

    FirebaseConfig  done

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyALse2PTTwh2QTpCq_vQKWhZBCZeOCwBH8",
+  authDomain: "chessclubjuan.firebaseapp.com",
+  projectId: "chessclubjuan",
+  storageBucket: "chessclubjuan.appspot.com",
+  messagingSenderId: "84526898536",
+  appId: "1:84526898536:web:fcb5351806591f5b7b4add",
+  measurementId: "G-Q37XVBQ42N"
+};
+const app = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
+console.log("firebase setup complete!");
+
+
 var stateUp = document.getElementById("state").value;
 var x = document.getElementById("myInput");
 var checkBox = document.getElementById("myCheck");
@@ -17,3 +31,4 @@ function myFunction() {
 function clicksubmitted() {
   prompt("Your data have been submitted succesfully");
 }
+
