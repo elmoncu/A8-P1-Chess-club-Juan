48c4b3dfe6fab4c85bea9708007399826b4aa8ab
elmoncu <164093323+elmoncu@users.noreply.github.com>
2024-05-06 14:39:31 +0000

    button onclick goBack

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function clicksubmitted() {
   prompt("Your data have been submitted succesfully");
 }
 
-function getStory(){
+function getStory() {
   var usEr = document.getElementById("usern").value;
   var stAte = document.getElementById("state").value;
   var zipCode = document.getElementById("zcode").value;
@@ -40,9 +40,9 @@ function getStory(){
   var emAiL = document.getElementById("myemail").value;
   var passW = document.getElementById("myInput").value;
   var storyName = document.getElementById("story").value;
-  console.log("story name:"+storyName);
-  document.getElementById("output").innerHTML= story;
-  console.log("output :" +story );
+  console.log("story name:" + storyName);
+  document.getElementById("output").innerHTML = story;
+  console.log("output :" + story);
 
   var storyData = {
     timestamp: Date.now(),
@@ -53,10 +53,10 @@ function getStory(){
     email: emAiL,
     password: passW,
     storyName: storyName,
-    }
-    return storyData;
   }
-function createMadLibstory(){
+  return storyData;
+}
+function createMadLibstory() {
   var storyData = getStory();
   console.log("storydata: " + storyData)
 }
@@ -72,88 +72,91 @@ function saveMadLib() {
   alert(storyData.storyName + " has been saved to the database");
 }
 
-function retrieveMadLib(){
+function retrieveMadLib() {
   //first, ask the used for the story name they want to retrieve
   var storyName = prompt("Enter the name of the story you want to retrieve (in console):"); db.collection("ChessClubJuan")
-  .doc(storyName)
-  .get()
-  .then((doc) => {
-    if (doc.exists) {
-      console.log("Document data:", doc.data());
-      var storyData = doc.data();
-      console.log("storyName: " + storyData.storyName);
-      document.getElementById("usern").innerHTML  = storyData.username;
-      document.getElementById("state").innerHTML = storyData.state;
-      document.getElementById("zcode").innerHTML = storyData.zipcode;
-      document.getElementById("phone").innerHTML = storyData.phone;
-      document.getElementById("myemail").innerHTML = storyData.email;
-      document.getElementById("myInput").innerHTML = storyData.password;
-      document.getElementById("story").innerHTML = storyData.storyName;
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-      document.getElementById("story").value = "Story not found";
-    }
-    //this method retrieves an existing story from the database to the console , not the webpage
-    console.log("retrieveMadLib: " + storyName);
-  })
-  .catch((error) => {
-    console.log("Error getting document:", error);
-    document.getElementById("story").innerHTML = "Story not found";
-  });
+    .doc(storyName)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        console.log("Document data:", doc.data());
+        var storyData = doc.data();
+        console.log("storyName: " + storyData.storyName);
+        document.getElementById("usern").innerHTML = storyData.username;
+        document.getElementById("state").innerHTML = storyData.state;
+        document.getElementById("zcode").innerHTML = storyData.zipcode;
+        document.getElementById("phone").innerHTML = storyData.phone;
+        document.getElementById("myemail").innerHTML = storyData.email;
+        document.getElementById("myInput").innerHTML = storyData.password;
+        document.getElementById("story").innerHTML = storyData.storyName;
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+        document.getElementById("story").value = "Story not found";
+      }
+      //this method retrieves an existing story from the database to the console , not the webpage
+      console.log("retrieveMadLib: " + storyName);
+    })
+    .catch((error) => {
+      console.log("Error getting document:", error);
+      document.getElementById("story").innerHTML = "Story not found";
+    });
 }
 function editMadLib() {
   //this method retrieves an existing story from the database to the webpage ( in the imput box)
   console.log("editMadLib: " + storyData);
   var storyData = prompt("Enter the name of the story you want to pop up in the form:"); db.collection("ChessClubJuan")
-  .doc(storyData)
-  .get()
-  .then((doc) => {
+    .doc(storyData)
+    .get()
+    .then((doc) => {
       if (doc.exists) {
         console.log("Document data: ", doc.data());
         var storyData = doc.data();
-        document.getElementById("usern").value  = storyData.username;
+        document.getElementById("usern").value = storyData.username;
         document.getElementById("state").value = storyData.state;
         document.getElementById("zcode").value = storyData.zipcode;
-        document.getElementById("phone").value  = storyData.phone;
+        document.getElementById("phone").value = storyData.phone;
         document.getElementById("myemail").value = storyData.email;
         document.getElementById("myInput").value = storyData.password;
-        document.getElementById("story").value  = storyData.storyName;
-    } else {
-      // doc.data () will be undefined in this case
-      console.log("No such document!");
-      document.getElementById("story").value = "Story not found";
-    }
-  })
-  .catch((error) => {
-    console.log("Error getting document:", error);
-    document.getElementById("story").innerHTML = "Story not found";
-  });
+        document.getElementById("story").value = storyData.storyName;
+      } else {
+        // doc.data () will be undefined in this case
+        console.log("No such document!");
+        document.getElementById("story").value = "Story not found";
+      }
+    })
+    .catch((error) => {
+      console.log("Error getting document:", error);
+      document.getElementById("story").innerHTML = "Story not found";
+    });
 }
 function deleteMadLib() {
   console.log("deleteMadLib() called : ");
-  var storyData = prompt("Enter the name of the story you want to delete:");db.collection("ChessClubJuan")
-  .doc(storyData)
-  .get()
-  .then((doc) => {
-    if(doc.exists){
-      console.log("Story Doc data: ", doc.data());
-      var storyData = doc.data();
-      document.getElementById("story").innerHTML= storyData.storyName + " has been deleted from the database"; db.collection("ChessClubJuan").doc(storyData.storyName).delete();
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-      document.getElementById("story").innerHTML  = "Story not found";
-    }
-  })
-  .catch((error) => {
-    console.log("Error getting document:", error);
-    document.getElementById("story").innerHTML = "Story not found";
-  });
-  }
-var windowRef=window.open("https://www.chess.com/home","_blank");
-function goBack() {
-  var window = window.open("index.html", "_self");
-  window.history.back()
+  var storyData = prompt("Enter the name of the story you want to delete:"); db.collection("ChessClubJuan")
+    .doc(storyData)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        console.log("Story Doc data: ", doc.data());
+        var storyData = doc.data();
+        document.getElementById("story").innerHTML = storyData.storyName + " has been deleted from the database"; db.collection("ChessClubJuan").doc(storyData.storyName).delete();
+      } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+        document.getElementById("story").innerHTML = "Story not found";
+      }
+    })
+    .catch((error) => {
+      console.log("Error getting document:", error);
+      document.getElementById("story").innerHTML = "Story not found";
+    });
 }
+
+//const location = <a href="index.html"></a>
+//function myFunction() {
+//location.assign(location);
+//}
+
+
+
 
